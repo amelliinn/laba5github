c49cc28e406856edfa2a7dfdc9025d55a89c2dad
amelliinn <swororows@gmail.com>
2023-11-23 02:07:07 +0700

    Добавьте файлы проекта.

---
+++ laboratornaya5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratornaya5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0;
+            double B = 0;
+
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1. Ввести А");
+                Console.WriteLine("2. Ввести В");
+                Console.WriteLine("3. Выполнить операцию «+»");
+                Console.WriteLine("4. Выполнить операцию «-»");
+                Console.WriteLine("5. Выполнить операцию «*»");
+                Console.WriteLine("6. Выполнить операцию «/»");
+                Console.WriteLine("7. Выход");
+
+                Console.Write("\nВыберите действие: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    default:
+                        Console.WriteLine("Неверный выбор. Пожалуйста, попробуйте еще раз.");
+                        break;
+                }
+            }
+        }
+    }
+}
